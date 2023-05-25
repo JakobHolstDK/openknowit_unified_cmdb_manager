b66071014666479631bbbe9b0f19e6dbfa0ac2d8
Jakob Holst <jho@miracle.dk>
2023-05-25 12:29:00 +0200

    user create

--- netbox.py
+++ netbox.py
@@ -8,3 +8,11 @@ nb = pynetbox.api(
     api,
     token=token
 )
+
+devices = nb.dcim.devices.all()
+for device in devices:
+  print(device.name)
+  print(device.primary_ip.address)
+  print(device.primary_ip.assigned_object.name)
+  print(device.primary_ip.assigned_object.device_role.name)
+  
